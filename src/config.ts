ae607cb580f2d1a83ebb5c3a6e8c8f3e9a341b0f
Christoph Stach <christoph.stach@gmail.com>
2022-09-23 12:36:28 +0200

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   subscribeUrl: 'https://andreasjansen.com/backend/subscribe.php',
   //subscribeUrl: 'http://localhost:5003/',
-  recommendationsLinkPrefix: 'https://andreasjansen.com/ergebnis/?',
+  recommendationsLinkPrefix: 'https://andreasjansen.com/resultat/?',
   // recommendationsLinkPrefix: 'http://localhost:5003/test.php?'
 };
