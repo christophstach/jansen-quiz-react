eac6a327c47a935ffa5f6259f31caa912fc1b6d5
Grossangriff <102793536+Grossangriff@users.noreply.github.com>
2022-09-24 16:10:07 +0200

    Icons Added
    Icons Added

--- src/atoms/categories.ts
+++ src/atoms/categories.ts
@@ -11,6 +11,7 @@ const categories: Category[] = [
   {
     id: '1',
     title: 'Alles klar! Los geht\'s',
+    icon: 'https://andreasjansen.com/wp-content/uploads/2022/03/Target.png',
     text: 'Welches dieser Ziele steht bei dir im Fokus?',
     parentId: '0',
   },
@@ -18,18 +19,21 @@ const categories: Category[] = [
   {
     id: '1.1',
     title: 'Meine Ausgaben senken',
+    icon: 'https://andreasjansen.com/wp-content/uploads/2022/09/2.png',
     finalizeCategoryText: 'Möchtest du auch deine Sparpotentiale ermitteln (empfohlen)?',
     parentId: '1',
   },
   {
     id: '1.2',
     title: 'Mein Vermögen aufbauen',
+    icon: 'https://andreasjansen.com/wp-content/uploads/2022/09/2.png',
     finalizeCategoryText: 'Möchtest du auch deine Investionschancen ermitteln (empfohlen)?',
     parentId: '1',
   },
   {
     id: '1.3',
     title: 'Absicherung für mich und meine Familie',
+    icon: 'https://andreasjansen.com/wp-content/uploads/2022/09/3.png',
     finalizeCategoryText: 'Möchtest du auch deine Absicherungslücken ermitteln (empfohlen)?',
     parentId: '1',
   },
