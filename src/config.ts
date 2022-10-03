1d058549a1869602ec6bca78cdb80409fc57f3e0
Grossangriff <102793536+Grossangriff@users.noreply.github.com>
2022-10-03 10:50:04 +0200

    Forward Link
    Forward Link

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,7 @@ export const config = {
   subscribeUrl: 'https://andreasjansen.com/finanzcheck-assets/send-quiz/',
   //subscribeUrl: 'http://localhost:5003/',
   recommendationsLinkPrefix: 'https://andreasjansen.com/resultat/?',
-  // recommendationsLinkPrefix: 'http://localhost:5003/test.php?'
+  // recommendationsLinkPrefix: 'http://localhost:5003/test.php?',
+  forwardlink: 'https://andreasjansen.com/verifizierung/',
+  // forwardlink: 'http://localhost:5003/test.php?'
 };
